d44bbc2755b9c01af7b2111a4b4a53c81c738389
Simon Lindsay <simon_lindsay@Simons-Laptop.local>
2025-04-20 21:38:12 +0100

    Fix: dynamic import shielding for Fleek build

--- src/icp_portfolio_frontend/src/backend/submitMessage.js
+++ src/icp_portfolio_frontend/src/backend/submitMessage.js
@@ -1,7 +1,8 @@
 export async function submitMessage(data) {
   if (import.meta.env.MODE === "development") {
     try {
-      const module = await import(
+      const dynamicImport = new Function("path", "return import(path);");
+      const module = await dynamicImport(
         "../../../declarations/icp_portfolio_backend"
       );
       const backend = module.icp_portfolio_backend;
